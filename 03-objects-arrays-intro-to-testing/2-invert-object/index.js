35b174f992a202b2cd11183431ad129fbdc27cc7
Ivan Pautov <ivan@machine.local>
2024-01-29 01:45:18 -0800

    Applied Code Review Suggestions for Task 2

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,7 +4,7 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-  if (!obj) return undefined;
+  if (!obj) return;
 
   const entries = Object.entries(obj);
   const swappedEntries = entries.map(entry => entry.reverse());
