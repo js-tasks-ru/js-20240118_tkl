381686f7f1bc4a0baccb9e24eacb8c12f7d04510
Ivan Pautov <ivan@machine.local>
2024-02-04 20:21:22 -0800

    Updated files

---
+++ 05-dom-document-loading/2-sortable-table-v1/Components/Cell.js
@@ -0,0 +1,18 @@
+export default class Cell {
+  #value;
+  #template;
+
+  constructor({ value = "", template: customTemplate }) {
+    this.#value = value;
+    this.#template = customTemplate ? customTemplate(this.#value) : this.#defaultTemplate;
+  }
+
+  renderTemplate() {
+    return this.#template;
+  }
+
+  get #defaultTemplate() {
+    return `<div class="sortable-table__cell">${this.#value}</div>`
+  }
+}
+
